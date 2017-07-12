de6667d01300a8824e367808201e54d4b4e45445
evoup <evoex123@gmail.com>
2017-07-12 17:40:58 +0800

    c

--- zookeeper/files/start.sh
+++ zookeeper/files/start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 
-service zookeeper-server init --force --myid="$@"
-#service zookeeper-server init
-service zookeeper-server start
 echo "server.1=zk_1:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
 echo "server.2=zk_2:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
 echo "server.3=zk_3:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
+#service zookeeper-server init
+service zookeeper-server init --force --myid="$@"
+service zookeeper-server start
 
 /usr/sbin/sshd -D
 
