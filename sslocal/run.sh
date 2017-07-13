719935f393db948eabfd9b6b22fb0529f21fb5f4
evoup <evoex123@gmail.com>
2017-07-13 14:48:07 +0800

    c

--- sslocal/run.sh
+++ sslocal/run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-docker run -d -p 10800:1080 wldf/docker-sslocal \
+docker run -d -p 10800:1080 --name sslocal wldf/docker-sslocal \
                 -b 0.0.0.0 \
                 -s us8.hxfq.in \
                 -p 16376 \
                 -l 1090 \
-                -k psword \
+                -k password \
                 -m rc4-md5
