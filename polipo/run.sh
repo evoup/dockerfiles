711498636e4192ae9b5310b92051ecdcfe7bab04
evoup <evoex123@gmail.com>
2017-07-14 17:32:03 +0800

    c

--- polipo/run.sh
+++ polipo/run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-docker run -d --name=polipo --net=host chenzhiwei/polipo \
-        proxyAddress=0.0.0.0 socksParentProxy=127.0.0.1:10800 socksProxyType=socks5
+#docker run -d --name=polipo --net=host chenzhiwei/polipo \
+        #proxyAddress=0.0.0.0 socksParentProxy=127.0.0.1:10800 socksProxyType=socks5
+
+docker run -d --name=polipo --net=b0 -p 8123:8123 --hostname=polipo chenzhiwei/polipo \
+        proxyAddress=0.0.0.0 socksParentProxy=192.168.31.142:10800 socksProxyType=socks5 proxyPort=8123
