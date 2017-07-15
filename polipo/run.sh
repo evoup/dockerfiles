2f9aa951038b38d99d9d8673b1a74d109b82fe40
evoup <yinjia@madhouse-inc.com>
2017-07-15 15:13:34 +0800

    c

--- polipo/run.sh
+++ polipo/run.sh
@@ -3,4 +3,4 @@
         #proxyAddress=0.0.0.0 socksParentProxy=127.0.0.1:10800 socksProxyType=socks5
 
 docker run -d --name=polipo --net=b0 -p 8123:8123 --hostname=polipo chenzhiwei/polipo \
-        proxyAddress=0.0.0.0 socksParentProxy=192.168.31.142:10800 socksProxyType=socks5 proxyPort=8123
+        proxyAddress=0.0.0.0 socksParentProxy=172.17.0.1:10800 socksProxyType=socks5 proxyPort=8123
