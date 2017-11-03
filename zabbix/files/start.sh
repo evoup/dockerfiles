29fb1d556dfd6bf0694c3eba02a002c40366d517
yinjia <evoex123@gmail.com>
2017-11-03 18:56:20 +0800

    c

--- zabbix/files/start.sh
+++ zabbix/files/start.sh
@@ -10,5 +10,7 @@ mysql --user=root --password=zabbix -e "flush privileges;"
 mysql --user=root --password=zabbix -e "use zabbix;source /services/software/zabbix-2.2.20/database/mysql/schema.sql;"
 mysql --user=root --password=zabbix -e "use zabbix;source /services/software/zabbix-2.2.20/database/mysql/data.sql;"
 mysql --user=root --password=zabbix -e "use zabbix;source /services/software/zabbix-2.2.20/database/mysql/images.sql;"
+cd /services/software/zabbix-2.2.20/ && ./configure --prefix=/usr/local/zabbix --enable-server --enable-agent --enable-proxy --with-mysql --enable-net-snmp --with-libcurl --enable-java
+make && make install
 
 /usr/sbin/sshd -D
