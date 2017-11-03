d04fb0a3fb9c2b7adc8c71572a0f67a7273e42c2
yinjia <evoex123@gmail.com>
2017-11-03 18:25:54 +0800

    c

--- zabbix/files/start.sh
+++ zabbix/files/start.sh
@@ -7,9 +7,8 @@ cat /tmp/hosts >> /etc/hosts
 mysql --user=root --password=zabbix -e "create database zabbix;"
 mysql --user=root --password=zabbix -e "grant all privileges on zabbix.* to zabbix@'%';"
 mysql --user=root --password=zabbix -e "flush privileges;"
-mysql --user=root --password=zabbix -e "use adsapi;"
-mysql --user=root --password=zabbix -e "source /services/software/zabbix-2.2.20/database/mysql/schema.sql;"
-mysql --user=root --password=zabbix -e "source /services/software/zabbix-2.2.20/database/mysql/data.sql;"
-mysql --user=root --password=zabbix -e "source /services/software/zabbix-2.2.20/database/mysql/images.sql;"
+mysql --user=root --password=zabbix -e "use zabbix;source /services/software/zabbix-2.2.20/database/mysql/schema.sql;"
+mysql --user=root --password=zabbix -e "use zabbix;source /services/software/zabbix-2.2.20/database/mysql/data.sql;"
+mysql --user=root --password=zabbix -e "use zabbix;source /services/software/zabbix-2.2.20/database/mysql/images.sql;"
 
 /usr/sbin/sshd -D
