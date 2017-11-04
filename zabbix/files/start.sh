88ca22651bcf8639b6a90a12f1069a971912d501
evoup <yinjia@madhouse-inc.com>
2017-11-04 23:19:49 +0800

    c

--- zabbix/files/start.sh
+++ zabbix/files/start.sh
@@ -12,11 +12,7 @@ cat /tmp/hosts >> /etc/hosts
 #mysql --user=root --password=zabbix -e "use zabbix;source /services/software/zabbix-2.2.20/database/mysql/images.sql;"
 #cd /services/software/zabbix-2.2.20/ && ./configure --prefix=/usr/local/zabbix --enable-server --enable-agent --enable-proxy --with-mysql --enable-net-snmp --with-libcurl --enable-java
 #make && make install
-mysql --user=root --password=zabbix -e "create database zabbix character set utf8 collate utf8_bin;"
-mysql --user=root --password=zabbix -e "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';"
-mysql --user=root --password=zabbix -e "use zabbix;source /usr/share/zabbix-mysql/schema.sql;"
-mysql --user=root --password=zabbix -e "use zabbix;source /usr/share/zabbix-mysql/data.sql;"
-mysql --user=root --password=zabbix -e "use zabbix;source /usr/share/zabbix-mysql/images.sql;"
+mysql --user=root --password=zabbix -e "create database zabbix character set utf8 collate utf8_bin;grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';use zabbix;source /usr/share/zabbix-mysql/schema.sql;source /usr/share/zabbix-mysql/images.sql;source /usr/share/zabbix-mysql/data.sql;"
 cat "\nDBPassword=zabbix\n" >> /etc/zabbix/zabbix_server.conf
 /etc/init.d/zabbix-server start
 /etc/init.d/httpd start
