7e8fddbf6a3150fafbde7781d6a2bff2592708ed
evoup <evoex123@gmail.com>
2017-11-18 17:32:35 +0800

    remove net dependency for polipo

--- sslocal/run.sh
+++ sslocal/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker run -d -p 10800:1080 --name sslocal wldf/docker-sslocal --ip 172.18.0.40 --net b0 \
+docker run -d -p 10800:1080 --name sslocal wldf/docker-sslocal \
                 -b 0.0.0.0 \
                 -s us8.hxfq.in \
                 -p 16376 \
@@ -7,11 +7,3 @@ docker run -d -p 10800:1080 --name sslocal wldf/docker-sslocal --ip 172.18.0.40 
                 -k password \
                 -m rc4-md5
 
-#without assign network mode:
-#docker run -d -p 10800:1080 --name sslocal wldf/docker-sslocal \
-                #-b 0.0.0.0 \
-                #-s us8.hxfq.in \
-                #-p 16376 \
-                #-l 1080 \
-                #-k password \
-                #-m rc4-md5
