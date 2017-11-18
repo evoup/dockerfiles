7990f3dcdb961b96200f8bcb620dffd3aaa293e8
evoup <evoex123@gmail.com>
2017-11-18 16:14:55 +0800

    add mine

--- polipo/run.sh
+++ polipo/run.sh
@@ -2,5 +2,5 @@
 #(first create network: docker network create --subnet=172.18.0.0/16 b0)
 #docker run -d --name=polipo --net=host chenzhiwei/polipo \
         #proxyAddress=0.0.0.0 socksParentProxy=127.0.0.1:10800 socksProxyType=socks5
-docker run -d --name=polipo --net=b0 -p 8123:8123 --hostname=polipo siji/polipo \
+docker run -d --name=polipo --net=b0 -p 8123:8123 --hostname=polipo evoup/polipo \
         proxyAddress=0.0.0.0 socksParentProxy=172.17.0.1:10800 socksProxyType=socks5 proxyPort=8123
