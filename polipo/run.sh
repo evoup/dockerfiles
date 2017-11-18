3173fe839515073e3a384189d817fffe1742d5d4
evoup <evoex123@gmail.com>
2017-11-18 15:46:17 +0800

    change to another docker image

--- polipo/run.sh
+++ polipo/run.sh
@@ -2,5 +2,5 @@
 #docker run -d --name=polipo --net=host chenzhiwei/polipo \
         #proxyAddress=0.0.0.0 socksParentProxy=127.0.0.1:10800 socksProxyType=socks5
 
-docker run -d --name=polipo --net=b0 -p 8123:8123 --hostname=polipo chenzhiwei/polipo \
+docker run -d --name=polipo --net=b0 -p 8123:8123 --hostname=polipo siji/polipo \
         proxyAddress=0.0.0.0 socksParentProxy=172.17.0.1:10800 socksProxyType=socks5 proxyPort=8123
