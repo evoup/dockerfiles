036e0b4112018d01cfeebf4bb75d75fc73ce148e
evoup <evoex123@gmail.com>
2017-11-18 17:10:41 +0800

    c

--- sslocal/run.sh
+++ sslocal/run.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-docker run -d -p 10800:1080 --name sslocal wldf/docker-sslocal \
+docker run -d -p 10800:1080 --name sslocal wldf/docker-sslocal --ip 172.18.0.40 --net b0 \
                 -b 0.0.0.0 \
                 -s us8.hxfq.in \
                 -p 16376 \
                 -l 1080 \
                 -k password \
                 -m rc4-md5
+
+#without assign network mode:
+#docker run -d -p 10800:1080 --name sslocal wldf/docker-sslocal \
+                #-b 0.0.0.0 \
+                #-s us8.hxfq.in \
+                #-p 16376 \
+                #-l 1080 \
+                #-k password \
+                #-m rc4-md5
