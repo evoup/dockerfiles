105f3524aaefab591c026baf4e2f600eab144d5c
yinjia <evoex123@gmail.com>
2017-11-24 14:48:21 +0800

    c

--- opentsdb/run.sh
+++ opentsdb/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+#cd /tmp/ && wget https://github.com/OpenTSDB/opentsdb/releases/download/v2.4.0RC2/opentsdb-2.4.0RC2.noarch.rpm && rpm -ivh opentsdb-2.4.0RC2.noarch.rpm
 docker run --privileged=true --name opentsdb1 -p14646:4646 -ti -d --ip 172.18.0.58 --net b0 --hostname opentsdb1 evoup/hadoop-datanode
