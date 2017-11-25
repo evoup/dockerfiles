982783b1193262c243e427ef75ec268fe8d881ab
evoup.dell-notebook <evoex123@gmail.com>
2017-11-25 14:48:41 +0800

    c

--- opentsdb/run.sh
+++ opentsdb/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-#yum install gnuplot
-#cd /tmp/ && wget https://github.com/OpenTSDB/opentsdb/releases/download/v2.4.0RC2/opentsdb-2.4.0RC2.noarch.rpm && rpm -ivh opentsdb-2.4.0RC2.noarch.rpm
-docker run --privileged=true --name opentsdb1 -p14646:4646 -ti -d --ip 172.18.0.58 --net b0 --hostname opentsdb1 evoup/hadoop-datanode
+docker run --privileged=true --name opentsdb1 -p14242:4242 -ti -d --ip 172.18.0.58 --net b0 --hostname opentsdb1 evoup/hadoop-datanode
+docker exec opentsdb1 yum install -y gnuplot
+docker exec opentsdb1 cd /tmp/ && wget https://github.com/OpenTSDB/opentsdb/releases/download/v2.4.0RC2/opentsdb-2.4.0RC2.noarch.rpm && rpm -ivh opentsdb-2.4.0RC2.noarch.rpm
