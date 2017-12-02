189ff51c0b0f0f974590c08b978df7c47f268e8c
evoup.dell-notebook <evoex123@gmail.com>
2017-12-02 14:43:32 +0800

    c

---
+++ kafka/run.sh
@@ -0,0 +1,1 @@
+docker run --privileged=true --name kafka -ti -d --ip 172.18.0.59 --net b0 --hostname kafka centos:6.9
--- uploadServer/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
- docker run --privileged=true --name uploadServer -p8005:80 -ti -d --ip 172.18.0.59 --net b0 --hostname uploadServer centos:6.9
