0defa0c465e3d1636521aeff043ad92b2617268c
evoup-home-win10 <evoex123@gmail.com>
2018-04-02 22:27:10 +0800

    windows下容器可以直接访问宿主机的ip，linux未知

--- polipo/run.sh
+++ polipo/run.sh
@@ -2,5 +2,9 @@
 #(first create network: docker network create --subnet=172.18.0.0/16 b0)
 #docker run -d --name=polipo --net=host chenzhiwei/polipo \
         #proxyAddress=0.0.0.0 socksParentProxy=127.0.0.1:10800 socksProxyType=socks5
+
+#docker run -d --name=polipo -p 8123:8123 --hostname=polipo evoup/polipo \
+#        proxyAddress=0.0.0.0 socksParentProxy=172.17.0.1:10800 socksProxyType=socks5 proxyPort=8123
+
 docker run -d --name=polipo -p 8123:8123 --hostname=polipo evoup/polipo \
-        proxyAddress=0.0.0.0 socksParentProxy=172.17.0.1:10800 socksProxyType=socks5 proxyPort=8123
+        proxyAddress=0.0.0.0 socksParentProxy=192.168.2.230:10800 socksProxyType=socks5 proxyPort=8123
