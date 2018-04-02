25f59c8b805854fb005d80637d4abb5133b34917
evoup-home-win10 <evoex123@gmail.com>
2018-04-02 21:57:01 +0800

    修改端口避免冲突

--- sslocal/run.sh
+++ sslocal/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker run -d -p 10800:1080 --name sslocal wldf/docker-sslocal \
+docker run -d -p 10800:1081 --name sslocal zhenkyle/docker-sslocal \
                 -b 0.0.0.0 \
                 -s us8.hxfq.in \
                 -p 16376 \
-                -l 1080 \
+                -l 1081 \
                 -k password \
                 -m rc4-md5
 
