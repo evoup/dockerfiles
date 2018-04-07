01af8619a52e2ad40b6ce73247a2cb3107bf681c
evoup.zhanqi <evoex123@gmail.com>
2018-04-07 18:55:58 +0800

    add note

--- hadoop-namenode/copyConf.sh
+++ hadoop-namenode/copyConf.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
-#copy conf.my_cluster to machine dir, conf.my_cluster is Customizing Configuration Files
+#before run hadoop hdfs, copy conf.my_cluster to machine dir, conf.my_cluster is Customizing Configuration Files
 
+
+docker cp customizedConf/namenode1/etc/hadoop/conf.my_cluster namenode1:/etc/hadoop/
+docker cp customizedConf/namenode1/etc/hbase/conf namenode1:/etc/hbase/
+
+
+docker cp customizedConf/datanode1/etc/hadoop/conf.my_cluster datanode1:/etc/hadoop/
+docker cp customizedConf/datanode1/etc/hbase/conf datanode1:/etc/hbase/
+
+docker cp customizedConf/datanode2/etc/hadoop/conf.my_cluster datanode2:/etc/hadoop/
+docker cp customizedConf/datanode2/etc/hbase/conf datanode2:/etc/hbase/
 
 
