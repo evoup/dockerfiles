b52b53bef21c2e46a77752b42f5a40ebe9b5e87d
evoup.zhanqi <evoex123@gmail.com>
2018-04-07 19:57:55 +0800

    change conf to conf.mu_cluster

--- hadoop-namenode/copyConf.sh
+++ hadoop-namenode/copyConf.sh
@@ -4,13 +4,13 @@
 
 
 docker cp customizedConf/namenode1/etc/hadoop/conf.my_cluster namenode1:/etc/hadoop/
-docker cp customizedConf/namenode1/etc/hbase/conf namenode1:/etc/hbase/
+#docker cp customizedConf/namenode1/etc/hbase/conf namenode1:/etc/hbase/
 
 
 docker cp customizedConf/datanode1/etc/hadoop/conf.my_cluster datanode1:/etc/hadoop/
-docker cp customizedConf/datanode1/etc/hbase/conf datanode1:/etc/hbase/
+#docker cp customizedConf/datanode1/etc/hbase/conf datanode1:/etc/hbase/
 
 docker cp customizedConf/datanode2/etc/hadoop/conf.my_cluster datanode2:/etc/hadoop/
-docker cp customizedConf/datanode2/etc/hbase/conf datanode2:/etc/hbase/
+#docker cp customizedConf/datanode2/etc/hbase/conf datanode2:/etc/hbase/
 
 
--- hadoop-namenode/installAddon.sh
+++ hadoop-namenode/installAddon.sh
@@ -5,3 +5,15 @@
 docker exec datanode1 yum install -y hadoop-mapreduce-historyserver hbase-regionserver hbase-thrift
 
 docker exec datanode2 yum install -y hbase-master hbase-regionserver hbase-thrift
+
+docker exec datanode1 alternatives --install /etc/hbase/conf hbase-conf /etc/hbase/conf.my_cluster 50
+docker exec datanode1 alternatives --set hbase-conf /etc/hbase/conf.my_cluster
+
+docker exec datanode2 alternatives --install /etc/hbase/conf hbase-conf /etc/hbase/conf.my_cluster 50
+docker exec datanode2 alternatives --set hbase-conf /etc/hbase/conf.my_cluster
+
+docker cp customizedConf/namenode1/etc/hbase/conf.my_cluster namenode1:/etc/hbase/
+
+docker cp customizedConf/datanode1/etc/hbase/conf.my_cluster datanode1:/etc/hbase/
+
+docker cp customizedConf/datanode2/etc/hbase/conf.my_cluster datanode2:/etc/hbase/
