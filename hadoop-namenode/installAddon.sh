cfb4b3a93c2c56ae3e62f332e42d7984f8f24573
evoup.zhanqi <evoex123@gmail.com>
2018-04-07 19:26:16 +0800

    add after install script

---
+++ hadoop-namenode/installAddon.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#this should be install after format hdfs and running hadoop
+
+docker exec datanode1 yum install -y hadoop-mapreduce-historyserver hbase-regionserver hbase-thrift
+
+docker exec datanode2 yum install hbase-master hbase-regionserver hbase-thrift
