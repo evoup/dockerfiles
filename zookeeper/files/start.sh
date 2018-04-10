91bfac57d20d68363ac8a92828e44951c812113b
evoup <evoex123@gmail.com>
2018-04-10 16:37:57 +0800

    expose jmx 18050 port

--- zookeeper/files/start.sh
+++ zookeeper/files/start.sh
@@ -5,6 +5,8 @@ cat /tmp/hosts >> /etc/hosts
 echo "server.1=zk1:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
 echo "server.2=zk2:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
 echo "server.3=zk3:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
+#echo "export SERVER_JVMFLAGS=\"-Djava.rmi.server.hostname=172.18.0.50 -Dcom.sun.management.jmxremote.port=18050 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false\"" >> /etc/default/zookeeper
+echo "export SERVER_JVMFLAGS=\"-Dcom.sun.management.jmxremote.port=18050 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false\"" >> /etc/default/zookeeper
 
 #service zookeeper-server init
 service zookeeper-server init --force --myid="$@"
