a4898018f7d0ff377fef422349f80778d8e24d46
evoup <evoex123@gmail.com>
2018-04-13 01:58:46 +0800

    add offical tomcat jmx support

--- tomcat/files/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-/usr/local/tomcat/bin/startup.sh
-/bin/bash
---
+++ tomcat/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+docker run -i -t -p 8200:8080 \
+    -p 9090:9090 \
+    --name tomcat \
+    -e CATALINA_OPTS="-Djava.rmi.server.hostname=192.168.2.196 \
+    -Dcom.sun.management.jmxremote \
+    -Dcom.sun.management.jmxremote.port=9090 \
+    -Dcom.sun.management.jmxremote.ssl=false \
+    -Dcom.sun.management.jmxremote.authenticate=false \
+    -Dcom.sun.management.jmxremote.rmi.port=9090" \
+    -d tomcat:8.0
