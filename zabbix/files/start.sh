6667473ebbc97e22657dbc54bbdf71ae8e1f2e79
evoup <evoex123@gmail.com>
2018-04-13 11:37:01 +0800

    add zabbix start param

--- zabbix/files/start.sh
+++ zabbix/files/start.sh
@@ -18,8 +18,17 @@ if [ "$RESULT" == "zabbix" ]; then
 else
     mysql --user=root --password=zabbix -e "create database zabbix character set utf8 collate utf8_bin;grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';use zabbix;source /usr/share/zabbix-mysql/schema.sql;source /usr/share/zabbix-mysql/images.sql;source /usr/share/zabbix-mysql/data.sql;"
     echo "DBPassword=zabbix" >> /etc/zabbix_server.conf
+    echo "# JavaGateway的所在服务器IP地址\nJavaGateway=127.0.0.1" >> /etc/zabbix_server.conf 
+    echo "# JavaGateway的服务端口\nJavaGatewayPort=10052" >> /etc/zabbix_server.conf 
+    echo "# 从javaGateway采集数据的进程数\nStartJavaPollers=5" >> /etc/zabbix_server.conf 
+    echo "# 监听地址\nLISTEN_IP=\"0.0.0.0\"" >> /etc/zabbix/zabbix_java_gateway.conf 
+    echo "# 监听端口\nLISTEN_PORT=10052" >> /etc/zabbix/zabbix_java_gateway.conf 
+    echo "# PID_FILE文件\nPID_FILE=\"/var/run/zabbix/zabbix_java.pid\"" >> /etc/zabbix/zabbix_java_gateway.conf 
+    echo "# 开启的工作线程数\nSTART_POLLERS=50" >> /etc/zabbix/zabbix_java_gateway.conf 
+    echo "# 超时时间\nTIMEOUT=3" >> /etc/zabbix/zabbix_java_gateway.conf 
 fi
 /etc/init.d/zabbix-server start
+/etc/init.d/zabbix-java-gateway/start
 /etc/init.d/httpd start
 
 /usr/sbin/sshd -D
