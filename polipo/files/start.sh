01ddf392b333814eedf0c719c5cb7d10208b9cec
evoup <evoex123@gmail.com>
2018-04-22 20:08:02 +0800

    c

--- polipo/files/start.sh
+++ polipo/files/start.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-polipo proxyAddress=${PROXY_ADDRESS} \
-       socksParentProxy=${SOCKS_PARENT_PROXY} \
-       socksProxyType=${SOCKS_PROXY_TYPE} \
-       proxyPort=${PROXY_PORT} \
-       dnsNameServer=${DNS_NAME_SERVER}
+#polipo proxyAddress=${PROXY_ADDRESS} \
+       #socksParentProxy=${SOCKS_PARENT_PROXY} \
+       #socksProxyType=${SOCKS_PROXY_TYPE} \
+       #proxyPort=${PROXY_PORT} \
+       #dnsNameServer=${DNS_NAME_SERVER}
+polipo proxyAddress=0.0.0.0 \
+       socksParentProxy=192.168.1.13:10800 \
+       socksProxyType=socks5 \
+       proxyPort=8124 \
+       dnsNameServer=8.8.8.8
