9602495bce239beb2083bee64cad6f1c3a914fe3
evoup <evoex123@gmail.com>
2018-04-22 17:37:42 +0800

    add readme

--- sslocal/files/start.sh
+++ sslocal/files/start.sh
@@ -2,8 +2,8 @@
 
 /usr/local/bin/sslocal \
     -b ${BIND_ADDRESS} \
-    -s ${SERVER_ADDRESS} \
-    -p ${SERVER_PORT} \
-    -l ${LISTEN_PORT} \
-    -k ${PASSWD} \
-    -m ${METHOD} 
+    -s ${SS_SERVER_ADDRESS} \
+    -p ${SS_SERVER_PORT} \
+    -l ${SOCKS5_PORT} \
+    -k ${SS_PASSWORD} \
+    -m ${ENCRYPTION_METHOD} 
