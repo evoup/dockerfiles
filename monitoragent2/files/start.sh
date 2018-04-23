8e314250352e4fe894fda44026123a516969e13a
evoup <evoex123@gmail.com>
2018-04-23 16:42:01 +0800

    auto build go file

--- monitoragent2/files/start.sh
+++ monitoragent2/files/start.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-
-#/usr/sbin/sshd -D
 /etc/init.d/rsyslog start
+if [ ! -d "/usr/local/lib/madmonitor2" ]; then
+    mkdir -p /usr/local/lib/madmonitor2
+    cd /root/go/src/madmonitor2
+    go build madmonitor2.go
+    go build --buildmode=plugin -o /usr/local/lib/madmonitor2/sysload.so collectors/sysload.go
+    go build --buildmode=plugin -o /usr/local/lib/madmonitor2/ifstat.so collectors/ifstat.go
+    go build --buildmode=plugin -o /usr/local/lib/madmonitor2/dfstat.so collectors/dfstat.go
+    go build --buildmode=plugin -o /usr/local/lib/madmonitor2/procstats.so collectors/procstats.go
+fi
 /bin/bash
