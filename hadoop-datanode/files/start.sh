efc1ec82095282098faab3f2a4bc2c82e196eae0
evoup.cop-macosx <evoex123@gmail.com>
2018-04-27 15:58:56 +0800

    add JAVA_HOME setting in init scription

--- hadoop-datanode/files/start.sh
+++ hadoop-datanode/files/start.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+if [ -d "/etc/hadoop/conf.my_cluster" ]
+then
+    echo "already inited"
+else
+    cat /tmp/hosts >> /etc/hosts
+    cp -r /etc/hadoop/conf.empty /etc/hadoop/conf.my_cluster
+    alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.my_cluster 50
+    alternatives --set hadoop-conf /etc/hadoop/conf.my_cluster
+    mkdir -p /data/1/dfs/dn /data/2/dfs/dn /data/3/dfs/dn /data/4/dfs/dn
+    chown -R hdfs:hdfs /data/1/dfs/dn /data/2/dfs/dn /data/3/dfs/dn /data/4/dfs/dn
+    echo "export JAVA_HOME=/usr/lib/jvm/java" >> /root/.bashrc
+fi
 
-cat /tmp/hosts >> /etc/hosts
-cp -r /etc/hadoop/conf.empty /etc/hadoop/conf.my_cluster
-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.my_cluster 50
-alternatives --set hadoop-conf /etc/hadoop/conf.my_cluster
-mkdir -p /data/1/dfs/dn /data/2/dfs/dn /data/3/dfs/dn /data/4/dfs/dn
-chown -R hdfs:hdfs /data/1/dfs/dn /data/2/dfs/dn /data/3/dfs/dn /data/4/dfs/dn
 /usr/sbin/sshd -D
-
--- hadoop-namenode/files/start.sh
+++ hadoop-namenode/files/start.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-
-cat /tmp/hosts >> /etc/hosts
-cp -r /etc/hadoop/conf.empty /etc/hadoop/conf.my_cluster
-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.my_cluster 50
-alternatives --set hadoop-conf /etc/hadoop/conf.my_cluster
-mkdir -p /data/1/dfs/nn /nfsmount/dfs/nn
-chown -R hdfs:hdfs /data/1/dfs/nn /nfsmount/dfs/nn
-
+if [ -d "/etc/hadoop/conf.my_cluster" ]
+    echo "already inited"
+then
+else
+    cat /tmp/hosts >> /etc/hosts
+    cp -r /etc/hadoop/conf.empty /etc/hadoop/conf.my_cluster
+    alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.my_cluster 50
+    alternatives --set hadoop-conf /etc/hadoop/conf.my_cluster
+    mkdir -p /data/1/dfs/nn /nfsmount/dfs/nn
+    chown -R hdfs:hdfs /data/1/dfs/nn /nfsmount/dfs/nn
+    echo "export JAVA_HOME=/usr/lib/jvm/java" >> /root/.bashrc
+fi
 
 /usr/sbin/sshd -D
-
