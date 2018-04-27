150bdf034328ca4d6b61f60e58048aa432bee977
evoup <evoex123@gmail.com>
2018-04-28 02:08:11 +0800

    comment opentsdb run script

--- opentsdb/run.sh
+++ opentsdb/run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-docker run --privileged=true --name opentsdb1 -p14242:4242 -ti -d --ip 172.18.0.58 --net b0 --hostname opentsdb1 evoup/hadoop-datanode
-docker exec opentsdb1 yum install -y gnuplot
-docker exec opentsdb1 bash -c "cd /tmp/ && wget https://github.com/OpenTSDB/opentsdb/releases/download/v2.4.0RC2/opentsdb-2.4.0RC2.noarch.rpm && rpm -ivh opentsdb-2.4.0RC2.noarch.rpm"
+# comment because need install hbase first, and must with jdk8
+#docker run --privileged=true --name opentsdb1 -p14243:4242 -ti -d --ip 172.18.0.58 --net b0 --hostname opentsdb1 evoup/hadoop-datanode
+#docker exec opentsdb1 yum install -y gnuplot
+#docker exec opentsdb1 bash -c "cd /tmp/ && wget -e \"http_proxy=192.168.2.197:8124\" https://github.com/OpenTSDB/opentsdb/releases/download/v2.4.0RC2/opentsdb-2.4.0RC2.noarch.rpm && rpm -ivh opentsdb-2.4.0RC2.noarch.rpm"
