0720fb8765779d10e8078696341cff52fe30c0a8
evoup.zhanqi <evoex123@gmail.com>
2018-04-28 20:58:50 +0800

    init zk

--- zookeeper/files/start.sh
+++ zookeeper/files/start.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-# if we don`t use swarm, also we can use bridge network 
-if [ -e "/usr/share/hosts" ]
-then
-    cat /usr/share/hosts >> /etc/hosts
+cat /hosts >> /etc/hosts
+
+if [ -d /usr/share/zooInited ] ; then
+else
+    echo "server.1=zk1:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
+    echo "server.2=zk2:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
+    echo "server.3=zk3:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
+    echo "maxClientCnxns=200" >> /etc/zookeeper/conf/zoo.cfg
+    #echo "export SERVER_JVMFLAGS=\"-Djava.rmi.server.hostname=172.18.0.50 -Dcom.sun.management.jmxremote.port=18050 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false\"" >> /etc/default/zookeeper
+    echo "export SERVER_JVMFLAGS=\"-Dcom.sun.management.jmxremote.port=18050 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false\"" >> /etc/default/zookeeper
+    #service zookeeper-server init
+    service zookeeper-server init --force --myid="$@"
+    mkdir -p /usr/share/zooInited
 fi
-
-echo "server.1=zk1:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
-echo "server.2=zk2:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
-echo "server.3=zk3:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
-echo "maxClientCnxns=200" >> /etc/zookeeper/conf/zoo.cfg
-#echo "export SERVER_JVMFLAGS=\"-Djava.rmi.server.hostname=172.18.0.50 -Dcom.sun.management.jmxremote.port=18050 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false\"" >> /etc/default/zookeeper
-echo "export SERVER_JVMFLAGS=\"-Dcom.sun.management.jmxremote.port=18050 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false\"" >> /etc/default/zookeeper
-
-#service zookeeper-server init
-service zookeeper-server init --force --myid="$@"
 service zookeeper-server start
 
 /usr/sbin/sshd -D
