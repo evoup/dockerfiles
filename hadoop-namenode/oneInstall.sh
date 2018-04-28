6c178562dea182aadb53f703e9e98387321723af
evoup <evoex123@gmail.com>
2018-04-28 23:37:34 +0800

    make oneInstall

---
+++ hadoop-namenode/oneInstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+. include/copyConf.sh
+. include/format.sh
+. include/startNode.sh
+. include/mkTmp.sh
+. include/installAddon.sh
+. include/startHbaseNode.sh
+. include/installOpenTsdb.sh
