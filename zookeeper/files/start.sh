d7785bbfc6cf4d14a4b1c515acd727afac523830
evoup <evoex123@gmail.com>
2018-04-30 18:58:50 +0800

    Revert "change create self subnet b0 to use default bridge network"
    This reverts commit bba0bab40f057fdb18e523441afa9130e9bb7c2e.

--- zookeeper/files/start.sh
+++ zookeeper/files/start.sh
@@ -9,7 +9,7 @@ else
     echo "server.2=zk2:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
     echo "server.3=zk3:2888:3888" >> /etc/zookeeper/conf/zoo.cfg
     echo "maxClientCnxns=200" >> /etc/zookeeper/conf/zoo.cfg
-    #echo "export SERVER_JVMFLAGS=\"-Djava.rmi.server.hostname=172.17.0.50 -Dcom.sun.management.jmxremote.port=18050 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false\"" >> /etc/default/zookeeper
+    #echo "export SERVER_JVMFLAGS=\"-Djava.rmi.server.hostname=172.18.0.50 -Dcom.sun.management.jmxremote.port=18050 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false\"" >> /etc/default/zookeeper
     echo "export SERVER_JVMFLAGS=\"-Dcom.sun.management.jmxremote.port=18050 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false\"" >> /etc/default/zookeeper
     #service zookeeper-server init
     service zookeeper-server init --force --myid="$@"
