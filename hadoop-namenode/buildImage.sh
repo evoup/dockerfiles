65dc9b21a578e5d14600bbba78b3200e0a44594f
evoup <evoex123@gmail.com>
2018-04-30 17:55:01 +0800

    fix env bug

--- hadoop-namenode/buildImage.sh
+++ hadoop-namenode/buildImage.sh
@@ -7,4 +7,4 @@ fi
 echo "network: ${network}"
 local_port=`docker ps|grep cdh5repo|sed 's/.*0.0.0.0://g'|sed 's/->.*//g'`
 echo "find cdh5repo local port: ${local_port}"
-docker build --network ${network} -t evoup/hadoop-namenode --build-arg cdh5repo_local_port=${local_port} . 
+docker build --network ${network} -t evoup/hadoop-namenode --build-arg CDH5REPO_LOCAL_PORT=${local_port} . 
