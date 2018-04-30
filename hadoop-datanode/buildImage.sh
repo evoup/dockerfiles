ccf50f60de6f4128d97686fc0ad0d3f4baaf09d4
evoup <evoex123@gmail.com>
2018-04-30 20:32:25 +0800

    add datanoded build script

---
+++ hadoop-datanode/buildImage.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+network=$1
+if [ -z "$network" ] ; then
+    echo "must provide a parameter for network, such as b0 or bridge"
+	exit
+fi
+echo "network: ${network}"
+local_port=`docker ps|grep cdh5repo|sed 's/.*0.0.0.0://g'|sed 's/->.*//g'`
+echo "find cdh5repo local port: ${local_port}"
+docker build --network ${network} -t evoup/hadoop-datanode --build-arg CDH5REPO_LOCAL_PORT=${local_port} . 
