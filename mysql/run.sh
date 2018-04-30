32757ccd514c5d465454bfbd7233227eff10f8f3
evoup <evoex123@gmail.com>
2018-04-30 18:58:50 +0800

    change create self subnet b0 to use default bridge network

--- mysql/run.sh
+++ mysql/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 #docker pull mysql
-#docker run -d -p 3306:3306 --name mysql --ip 172.18.0.60 --net b0 mysql
+#docker run -d -p 3306:3306 --name mysql --ip 172.17.0.60 --net b0 mysql
 docker run --name mysql -e MYSQL_ROOT_PASSWORD=passwd -d -p3306:3306 mysql
