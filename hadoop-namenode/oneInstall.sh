a2581e246722ea3650b364a5d1061a6b18d725bd
evoup <evoex123@gmail.com>
2018-04-30 21:17:19 +0800

    add pkg provider

--- hadoop-namenode/oneInstall.sh
+++ hadoop-namenode/oneInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 docker start pkgrepo
-docker run --privileged=true --name namenode1 -p2206:22 -p50070:50070 -ti -d --ip 172.18.0.53 --net b0 --hostname namenode1 evoup/hadoop-namenode
-docker run --privileged=true --name datanode1 -p2207:22 -p19888:19888 -p14242:4242 -ti -d --ip 172.18.0.54 --net b0 --hostname datanode1 evoup/hadoop-datanode
-docker run --privileged=true --name datanode2 -p2208:22 -ti -d --ip 172.18.0.55 --net b0 --hostname datanode2 evoup/hadoop-datanode
+docker run --privileged=true --name namenode1 -p50070:50070 -ti -d --ip 172.18.0.53 --net b0 --hostname namenode1 evoup/hadoop-namenode
+docker run --privileged=true --name datanode1 -p19888:19888 -p14242:4242 -ti -d --ip 172.18.0.54 --net b0 --hostname datanode1 evoup/hadoop-datanode
+docker run --privileged=true --name datanode2 -ti -d --ip 172.18.0.55 --net b0 --hostname datanode2 evoup/hadoop-datanode
 sleep 10
 echo "ok"
 . include/copyConf.sh
