1b8f9e9f26fa51fd8f29624fa462556de242dd95
evoup <evoex123@gmail.com>
2018-05-16 23:14:27 +0800

    add redis expose port 6379

--- redis/run.sh
+++ redis/run.sh
@@ -3,4 +3,4 @@
 #docker run --name some-redis -d redis
 
 #persistent mode
-docker run --name redis -d redis redis-server --appendonly yes
+docker run --name redis -p6379:6379 -d redis redis-server --appendonly yes
