d59a01ee7e905ec53bb9ef88a03728c33f3c1d0a
evoup <evoex123@gmail.com>
2018-06-18 20:04:33 +0800

    update nexus run shell

--- nexus/run.sh
+++ nexus/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker pull sonatype/nexus:oss
-docker run -d -p 8081:8081 --name nexus sonatype/nexus:oss
+docker run -d -p 8081:8081 --name nexus --net b0 --hostname nexus sonatype/nexus:oss
