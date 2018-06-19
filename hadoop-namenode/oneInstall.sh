d2d3228fead94aa87c3cdf0a77605fc1995f1a74
evoup.zhanqi <evoex123@gmail.com>
2018-06-19 21:35:42 +0800

    update thrift port to 9090

--- hadoop-namenode/oneInstall.sh
+++ hadoop-namenode/oneInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker start pkgrepo
 docker run --privileged=true --name namenode1 -p50070:50070 -ti -d --ip 172.18.0.53 --net b0 --hostname namenode1 evoup/hadoop-namenode
-docker run --privileged=true --name datanode1 -p19888:19888 -p14242:4242 -p8090:8090 -P -ti -d --ip 172.18.0.54 --net b0 --hostname datanode1 evoup/hadoop-datanode
+docker run --privileged=true --name datanode1 -p19888:19888 -p14242:4242 -p9090:9090 -p8090:8090 -P -ti -d --ip 172.18.0.54 --net b0 --hostname datanode1 evoup/hadoop-datanode
 docker run --privileged=true --name datanode2 -P -ti -d --ip 172.18.0.55 --net b0 --hostname datanode2 evoup/hadoop-datanode
 sleep 10
 echo "ok"
