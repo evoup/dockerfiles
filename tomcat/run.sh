dfef4b325b63505024bb68161464754355878b75
evoup <evoex123@gmail.com>
2018-06-19 16:34:31 +0800

    add note for tomcat dockerfile

--- tomcat/run.sh
+++ tomcat/run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+#-Djava.rmi.server.hostname=10.1.1.118 is host ip, not docker container ip
 docker run -i -t -p 8200:8080 \
     -p 9090:9090 \
     --name tomcat \
-    -e CATALINA_OPTS="-Djava.rmi.server.hostname=10.1.1.142 \
+    -e CATALINA_OPTS="-Djava.rmi.server.hostname=10.1.1.118 \
     -Dcom.sun.management.jmxremote \
     -Dcom.sun.management.jmxremote.port=9090 \
     -Dcom.sun.management.jmxremote.ssl=false \
