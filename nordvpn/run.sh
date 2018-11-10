412a6d0a564a187b50328db08ff93ed4da42cdb5
evoup <evoex123@gmail.com>
2018-11-10 14:37:51 +0800

    c

---
+++ nordvpn/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#docker run -ti --cap-add=NET_ADMIN --device /dev/net/tun --name vpn0\
+            #-e USER=user@email.com -e PASS=password
+            #-e COUNRTY="country1;country2" -e CATEGORY="Obfuscated Servers" \
+            #-e PROTOCOL=protocol -d bubuntux/nordvpn
+
+docker run -ti --cap-add=NET_ADMIN --device /dev/net/tun --name vpn0\
+            -e USER=user@email.com -e PASS=password
+            -e CATEGORY="Obfuscated Servers" \
+            -d evoup/nordvpn
