1d3a5081f9bf832fc0eb86b781b5bc21ffb5ec3d
evoup <evoex123@gmail.com>
2019-01-20 00:45:17 +0800

    c

--- shadowsocks-libev/run.sh
+++ shadowsocks-libev/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -e PASSWORD=password -p 8388:8388 -p 8388:8388/udp -m chacha20 -d --restart always shadowsocks/shadowsocks-libev
+docker run -e PASSWORD=password -e METHOD=chacha20 -p 8388:8388 -p 8388:8388/udp -d --restart always shadowsocks/shadowsocks-libev
