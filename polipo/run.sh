eb93637c8b50d8628007c1085d8896e45ee44509
evoup.dell-notebook <evoex123@gmail.com>
2019-04-27 22:34:47 +0800

    fix bug

--- polipo/run.sh
+++ polipo/run.sh
@@ -2,11 +2,11 @@
 #(may be first create network: docker network create --subnet=172.18.0.0/16 b0)
 
 
-docker run -d --name=polipo -p 8124:8124 --hostname=polipo -e PROXY_ADDRESS="0.0.0.0" \
-                             -e SOCKS_PARENT_PROXY="192.168.1.13:10800" \
-                             -e SOCKS_PROXY_TYPE="socks5" \
-                             -e PROXY_PORT="8124" \
-                             -e DNS_NAME_SERVER="8.8.8.8" \
-                             --restart always \
-                             --name polipo evoup/polipo
+docker run -d -p 8124:8124 --hostname=polipo -e PROXY_ADDRESS="0.0.0.0" \
+    -e SOCKS_PARENT_PROXY="192.168.1.13:10800" \
+    -e SOCKS_PROXY_TYPE="socks5" \
+    -e PROXY_PORT="8124" \
+    -e DNS_NAME_SERVER="8.8.8.8" \
+    --restart always \
+    --name polipo evoup/polipo
 
