f3fbe7ada2e404290e14855b544767dc6cb22ecf
evoup.dell-notebook <evoex123@gmail.com>
2019-05-03 23:22:13 +0800

    add es network

--- elasticsearch/run.sh
+++ elasticsearch/run.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 docker pull elasticsearch:5.6.15
+docker network create es-network
+docker run -d --name elasticsearch --net es-network -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" elasticsearch:5.6.15
+
