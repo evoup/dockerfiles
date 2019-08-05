e7a67f35a402efa40a0bbaa12b4f90911e9f26fd
evoup.xiaomi-notebook <evoex123@gmail.com>
2019-08-05 15:06:32 +0800

    update

--- nexus/run.sh
+++ nexus/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker pull sonatype/nexus:oss
-docker run -d -p 8081:8081 --name nexus --net b0 --hostname nexus sonatype/nexus:oss
+docker run --restart always -d -p 8081:8081 --name nexus --net b0 --hostname nexus sonatype/nexus:oss
