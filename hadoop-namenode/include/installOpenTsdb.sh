1a44bcc83e351a5aa59b394367941ba5c305478d
evoup.dell-notebook <evoex123@gmail.com>
2019-08-07 23:29:30 +0800

    修正opentsdb配置，支持多个zk节点

--- hadoop-namenode/include/installOpenTsdb.sh
+++ hadoop-namenode/include/installOpenTsdb.sh
@@ -11,5 +11,5 @@ docker exec datanode1 yum install -y gnuplot
 #docker exec datanode1 bash -c "rpm -ivh http://${docker_host_ip}:${local_port}/pkg/opentsdb-2.4.0RC2.noarch.rpm"
 docker exec datanode1 bash -c "rpm -ivh http://${docker_host_ip}:${local_port}/pkg/opentsdb-2.3.1.noarch.rpm"
 docker exec datanode1 env COMPRESSION=NONE JAVA_HOME=/usr/lib/jvm/java HBASE_HOME=/usr/lib/hbase /usr/share/opentsdb/tools/create_table.sh
-docker exec datanode1 sh -c "echo \"tsd.storage.hbase.zk_quorum = zk1\" >> /etc/opentsdb/opentsdb.conf"
+docker exec datanode1 sh -c "echo \"tsd.storage.hbase.zk_quorum = \"zk1,zk2,zk3\"\" >> /etc/opentsdb/opentsdb.conf"
 docker exec datanode1 sh -c "/etc/init.d/opentsdb start"
