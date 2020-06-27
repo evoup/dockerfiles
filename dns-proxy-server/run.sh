5358dcb42045bed88f27cc14722594c5eb51d9ea
evoup.dell-notebook <evoex123@gmail.com>
2020-06-27 17:40:27 +0800

    update

--- dns-proxy-server/run.sh
+++ dns-proxy-server/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-docker run -d --hostname dns.mageddo --restart=unless-stopped -p 5380:5380 \
+docker run -d --name dns-proxy-server --hostname dns.mageddo --restart=unless-stopped -p 5380:5380 \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v /etc/resolv.conf:/etc/resolv.conf \
     defreitas/dns-proxy-server
