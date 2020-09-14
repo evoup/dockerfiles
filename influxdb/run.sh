46b34b92e67840dd527701c0eed92d2e087874b5
evoup.xiaomi-notebook <evoex123@gmail.com>
2020-09-14 13:52:41 +0800

    update

--- influxdb/run.sh
+++ influxdb/run.sh
@@ -3,3 +3,7 @@ docker run --name influxdb -dp 8086:8086 \
       -v $PWD:/var/lib/influxdb \
       influxdb:1.7.0
 
+# The administrator interface is deprecated as of 1.1.0 and will be removed in 1.3.0. It is disabled by default. If needed, it can still be enabled by setting an environment variable like below:
+#docker run --name influxdb -e INFLUXDB_ADMIN_ENABLED=true -d -p 8083:8083 -p 8086:8086 \
+      #-v $PWD:/var/lib/influxdb \
+      #influxdb:1.7.0
